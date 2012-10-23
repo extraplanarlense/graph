e7ad5155d24cf2415c2fee289d7b3ff0ee80be08
Robert Baumeister <robert.baumeister@siebelmann.de>
2012-10-23 15:16:28 +0200

    Define Graph API

---
+++ graph/__init__.py
@@ -0,0 +1,1 @@
+from .base import Graph, Node, Edge
---
+++ graph/base.py
@@ -0,0 +1,72 @@
+class Graph:
+
+    @property
+    def properties(self):
+        """Mapping of properties assigned to the graph."""
+        raise NotImplementedError
+
+    def nodes(self):
+        """Make an iterable of all nodes in the graph."""
+        raise NotImplementedError
+
+    def edges(self):
+        """Make an iterable of all edges in the graph."""
+        raise NotImplementedError
+
+    def add_node(self):
+        """Add a node to the graph.
+
+        Returns the newly created node.
+        """
+        raise NotImplementedError
+
+    def remove_node(self, node):
+        """Remove a given node from the graph."""
+        raise NotImplementedError
+
+    def add_edge(self, source, target):
+        """Add an edge between two nodes."""
+        raise NotImplementedError
+
+    def remove_edge(self, edge):
+        """Remove an edge from the graph."""
+        raise NotImplementedError
+
+
+class Node:
+
+    @property
+    def properties(self):
+        """Mapping of properties assigned to the node."""
+        raise NotImplementedError
+
+    def inbound(self):
+        """Make an iterable of all edges that end at the node."""
+        raise NotImplementedError
+
+    def outbound(self):
+        """Make an iterable of all edges that start at the node."""
+        raise NotImplementedError
+
+    def edges(self):
+        """Make an iterable of all edges that start or end at the node."""
+        raise NotImplementedError
+
+
+class Edge:
+    """Connection between two nodes."""
+
+    @property
+    def properties(self):
+        """Mapping of properties assigned to the edge."""
+        raise NotImplementedError
+
+    @property
+    def target(self):
+        """Return the node at which the edge ends."""
+        raise NotImplementedError
+
+    @property
+    def source(self):
+        """Return the node at which the edge starts."""
+        raise NotImplementedError
