bf528c4625af0274c1dd8fc5e23a19f286211cae
Robert Baumeister <robert.baumeister@siebelmann.de>
2012-10-24 13:30:44 +0200

    Implement serialization into Trivial Graph Format

---
+++ graph/io/tgf.py
@@ -0,0 +1,21 @@
+from .base import GraphSerializer
+
+
+class TGFSerializer(GraphSerializer):
+    """Serializer for the Trivial Graph Format."""
+
+    def __init__(self, node_label=None, edge_label=None):
+        self.node_label = node_label
+        self.edge_label = edge_label
+
+    def iter_serialize(self, graph):
+        """Serialize a graph, yielding parts of the result when available."""
+        ids = dict()
+        for id, node in enumerate(graph.nodes()):
+            yield str(id) + " " + str(node.properties[self.node_label]) + "\n"
+            ids[node] = id
+        yield "#\n"
+        for edge in graph.edges():
+            yield (str(ids[edge.source]) + " " + str(ids[edge.target]) + " "
+                   + str(edge.properties[self.edge_label]) + "\n")
+        yield "\n"
